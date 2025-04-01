afd2d5db4949001bc4fb574771564e72afefa7e7
sheeba bangera <sheeba.bangera@codecraft.co.in>
2025-04-01 12:14:09 +0530

    home controller change

--- src/main/java/com/example/QBConnectorService/HomeController.java
+++ src/main/java/com/example/QBConnectorService/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/services/qbwService")
+@RequestMapping("/")
 public class HomeController {
 
     @GetMapping(produces = MediaType.TEXT_XML_VALUE)
