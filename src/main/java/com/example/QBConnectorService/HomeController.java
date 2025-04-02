bcfe78749070f71029e51355b03172cd9cbd662d
sheeba bangera <sheeba.bangera@codecraft.co.in>
2025-04-02 12:27:42 +0530

    change home controller 3

--- src/main/java/com/example/QBConnectorService/HomeController.java
+++ src/main/java/com/example/QBConnectorService/HomeController.java
@@ -1,12 +1,15 @@
 package com.example.QBConnectorService;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -34,12 +37,12 @@ import java.io.IOException;
 //    }
 //}
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
-    @GetMapping
-    public String forwardToSoapService() {
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+    public String forwardToSoapService(HttpServletRequest request) {
         return "forward:/services/qbwService";
     }
 }
